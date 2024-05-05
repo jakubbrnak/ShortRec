96da929cedad0cb4993f42828d9cf5de90363a44
Jakub012 <jakub@brnak.sk>
2024-05-05 15:13:09 +0200

    Initial Commit

---
+++ ShortRec/ShortRecApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShortRecApp.swift
+//  ShortRec
+//
+//  Created by Jakub Brnák on 05/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct ShortRecApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
